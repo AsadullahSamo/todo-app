1b2a24c587aaf0647490b82b62c55bbd3f6e4415
AsadullahSamo <109850878+AsadullahSamo@users.noreply.github.com>
2024-01-28 00:37:28 +0500

    Add line-through functionality

--- index.js
+++ index.js
@@ -7,28 +7,59 @@ let checkedMap = new Map([])
 const tasks = document.getElementById('tasks')
 const remainingTasks = document.getElementById('remaining-tasks')
 const totalTasks = document.getElementById('total-tasks')
+const todoModeImg = document.getElementById('mode');
+const inputTodoList = document.querySelector('.todo-card label input.todo');
 
 //  TODO BUTTON
 const todoButton = document.querySelector('.todo-button');
-todoButton.addEventListener('click', function (e) {
-  createTodoList();
-});
-
 const todo = document.querySelector('.todo');
 const todoListDiv = document.querySelector('.todo-list-div');
+const emptyTodoMsg = document.getElementById('empty-todo-msg');
+
+const updateTodoButtonState = () => {
+
+}
+
+todo.addEventListener('input', updateTodoButtonState)
+
+todoButton.addEventListener('click', function () {
+  if (todo.value.length > 0) {
+    createTodoList();
+  } else {
+    emptyTodoMsg.style.display = 'block'
+    setTimeout(() => {
+      emptyTodoMsg.style.display = 'none'
+    }, 1500)
+  }
+});
 
 window.addEventListener('keyup', function (e) {
-  if (e.key === 'Enter') {
-    createTodoList();
+  if (todo.value.length > 0) {
+    if (e.key === 'Enter') {
+      createTodoList();
+    }
   }
 })
 
+const map = new Map(JSON.parse(localStorage.getItem('checked')))
+const cactusTodoCaption = document.getElementById('cactus-todo-caption');
+
 const createTodoList = (value) => {
+  if (todoListArray.includes(todo.value)) {
+    emptyTodoMsg.style.display = 'block'
+    emptyTodoMsg.innerHTML = `${todo.value} already exists`
+    setTimeout(() => {
+      emptyTodoMsg.innerHTML = `Empty task can't be entered. Please enter a task`
+      emptyTodoMsg.style.display = 'none'
+    }, 1500)
+    return
+  }
   // <label for= "todo" class= "todo-list-label" >
-  //   <input type="checkbox" name="task-status" id="task-status"></input>
+  //   <input type="checkbox" name="task-status" id="task-status" disabled></input>
   //   <input type="text" name="todo" class="todo-list" value="A">
   //   <img src="./assets/icons/close.svg" class="close-icon" alt="Close icon">
   // </label> 
+
   let inputValue = String(todo.value).trim();
 
   const label = document.createElement('label');
@@ -38,93 +69,194 @@ const createTodoList = (value) => {
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
   checkbox.setAttribute('name', 'task-status');
-  checkbox.className = "task-status hide-appearance"
+  checkbox.className = "task-status"
+
+  if (map.get(`${value}`)) {
+    checkbox.className += " appearance"
+    checkbox.setAttribute('checked', 'true')
+  } else {
+    checkbox.className += " hide-appearance"
+  }
 
   const input = document.createElement('input');
   input.classList.add('todo-list');
-  input.cssText = `type: text; name: todo; class: todo-list; value: ${todo.value};`;
+  input.setAttribute('type', 'text');
+  input.setAttribute('name', 'todo');
+  input.setAttribute('disabled', 'true')
+
+  if (todoModeImg.src.includes('light-mode')) {
+    input.setAttribute('style', 'border: 1px solid #fff; color: #fff');
+  } else {
+    input.setAttribute('style', 'border: 1px solid #000; color: #000');
+  }
   input.value = inputValue || value;
 
-  const img = document.createElement('img');
-  img.src = "./assets/icons/close.svg";
-  img.classList.add('close-icon');
-  img.setAttribute('alt', 'Close icon');
+  const pencilImg = document.createElement('img');
+  pencilImg.src = "./assets/icons/pencil.svg";
+  pencilImg.classList.add('pencil-icon');
+  pencilImg.setAttribute('alt', 'Pencil icon');
+
+  const closeImg = document.createElement('img');
+  closeImg.src = "./assets/icons/close.svg";
+  closeImg.classList.add('close-icon');
+  closeImg.setAttribute('alt', 'Close icon');
 
   // Append elements
   label.appendChild(checkbox);
   label.appendChild(input);
-  label.appendChild(img);
-  todoListDiv.appendChild(label);
+  label.appendChild(pencilImg);
+  label.appendChild(closeImg);
 
+  if (checkbox.classList.contains('appearance')) {
+    checkbox.nextElementSibling.classList.add('line-through')
+    todoListDiv.prepend(label);
+  } else {
+    todoListDiv.appendChild(label);
+  }
 
   todoListArray.push(input.value);
   todoListArray = [...new Set(todoListArray)];
 
-  checkedMap.set(`${input.value}`, false)
+  if (map.get(`${value}`)) {
+    checkedMap.set(`${input.value}`, true)
+  } else {
+    checkedMap.set(`${input.value}`, false)
+  }
 
   if (value === undefined) {
-    localStorage.setItem('todoList', JSON.stringify(todoListArray));
-    localStorage.setItem('checked', JSON.stringify(Array.from(checkedMap.entries())))
+    updateLocalStorage();
   }
 
   updateTodoCardHeight('add');
   showTasksStatus()
   showCactusTodo();
+  todo.value = '';   // Clear input field
 }    // end of createTodoList() function
 
 // Update todo card height based on todo list length dynamically
 const updateTodoCardHeight = (operator) => {
-  operator === "add" ? todoCardHeight += 50 : todoCardHeight -= 50;
+  operator === "add" ? todoCardHeight += 49 : todoCardHeight -= 49;
   todoCard.style.height = `${todoCardHeight}px`;
 }    // end of updateTodoCardHeight() function
 
 // Show cactus todo image when todo list is empty
 const showCactusTodo = () => {
   const cactusTodo = document.getElementById('cactus-todo');
-  todoListArray.length === 0 ? cactusTodo.style.display = 'block' : cactusTodo.style.display = 'none';
+  if (todoListArray.length === 0) {
+    cactusTodo.style.display = 'block'
+    cactusTodoCaption.style.display = 'block'
+  } else {
+    cactusTodo.style.display = 'none'
+    cactusTodoCaption.style.display = 'none'
+  }
+  // todoListArray.length === 0 ? cactusTodo.style.display = 'block' && cactusTodoCaption.style : cactusTodo.style.display = 'none';
 }    // end of showCactusTodo() function
 
 const showTasksStatus = () => {
-  // let remainingTasksArray = checkedMap.filter
-  if (todoListArray.length === 0) {
-    totalTasks.textContent = 0;
-  } else {
-    totalTasks.textContent = todoListArray.length;
-  }
+  totalTasks.textContent = todoListArray.length;
 }
-
 
 // Add remove functionality to todo list
 todoListDiv.addEventListener('click', function (e) {
+  // Remove todo list
   if (e.target.classList.contains('close-icon')) {
     e.target.parentElement.remove();
-    todoListArray = todoListArray.filter((todo) => todo !== e.target.previousElementSibling.value);
+    todoListArray = todoListArray.filter((todo) => todo !== e.target.previousElementSibling.previousElementSibling.value);
     updateTodoCardHeight('remove');
     showTasksStatus()
     showCactusTodo();
 
     remainingTasks.textContent = document.querySelectorAll('.appearance').length
-    localStorage.setItem('todoList', JSON.stringify(todoListArray));
-  }
+    checkedMap.delete(`${e.target.previousElementSibling.previousElementSibling.value}`)
+    updateLocalStorage();
+  } // end of remove todo list
 
+  if (e.target.classList.contains('pencil-icon')) {
+    const checkedArray = Array.from(checkedMap.values())
+    localStorage.removeItem('checked')
 
+    const inputField = e.target.previousElementSibling;
+    const index = todoListArray.findIndex(item => item === inputField.value);
+    inputField.removeAttribute('disabled');
+    inputField.focus();
 
+    inputField.addEventListener('keyup', function (e) {
+      if (e.key === 'Enter') {
+        handleEditTodoList(e)
+
+      }
+
+      inputField.addEventListener('blur', function (e) {
+        handleEditTodoList(e)
+      })
+    }) // end of inputField event listener
+
+    const handleEditTodoList = (e) => {
+      checkedMap.clear()
+      inputField.setAttribute('disabled', 'true');
+
+      todoListArray.splice(index, 1, inputField.value);
+      todoListArray = [...new Set(todoListArray)];
+
+      for (let i = 0; i < todoListArray.length; i++) {
+        if (inputField.value === todoListArray[i]) {
+          checkedMap.set(`${todoListArray[i]}`, inputField.previousElementSibling.classList.contains('appearance'))
+        } else {
+          checkedMap.set(`${todoListArray[i]}`, checkedArray[i])
+        }
+      }
+
+      updateLocalStorage();
+      inputField.blur();
+    } // end of handleEditTodoList() function
+
+  } // end of edit todo list
+
+  // Complete/Incomplete task
   if (e.target.classList.contains(`task-status`)) {
-    e.target.nextElementSibling.classList.toggle('line-through')
     e.target.classList.toggle('appearance');
     e.target.classList.toggle('hide-appearance');
-
-    checkedMap.set(`${e.target.nextElementSibling.value}`, true)
-    localStorage.setItem('checked', JSON.stringify(Array.from(checkedMap.entries())))
+    e.target.nextElementSibling.classList.toggle('line-through')
 
     if (e.target.classList.contains('appearance')) {
+      checkedMap.set(`${e.target.nextElementSibling.value}`, true)
       remainingTasks.textContent = Number(remainingTasks.textContent) + 1
     } else {
+      checkedMap.set(`${e.target.nextElementSibling.value}`, false)
       remainingTasks.textContent = Number(remainingTasks.textContent) - 1
     }
-  }
 
-})
+    localStorage.setItem('checked', JSON.stringify(Array.from(checkedMap.entries())))
+    location.reload()
+  } // end of complete/incomplete task
+
+}) // end of todoListDiv event listener
+
+// Light/Dark mode
+todoModeImg.addEventListener('click', function (e) {
+  if (todoModeImg.src.includes('light-mode')) {
+    changeTodoMode('./assets/icons/dark-mode.svg', '#000', '#fff', '#000', 'none')
+  } else {
+    changeTodoMode('./assets/icons/light-mode.svg', '#fff', '#24273D', '#fff', 'block')
+  }
+})   // end of todoModeImg event listener
+
+const changeTodoMode = (modeImg, color, bgColor, borderColor, showCactusTodo) => {
+  todoCard.style.backgroundColor = bgColor;
+  todo.style.cssText = `border: 1px solid ${borderColor}; color: ${color};`;
+  tasks.style.cssText = `color: ${color};`
+  todoModeImg.src = modeImg;
+
+  let todoList = Array.from(document.getElementsByClassName('todo-list'))
+  todoList.forEach((todo) => {
+    todo.style.cssText = `border: 1px solid ${borderColor}; color: ${color};`;
+  });
+
+  const cactusTodo = document.getElementById('cactus-todo');
+  cactusTodo.style.display = `${showCactusTodo}`
+
+  cactusTodoCaption.style.color = `${color}`
+}   // end of changeTodoMode() function
 
 //  Load todoListArray from localStorage on page load
 if (localStorage.length > 0) {
@@ -132,7 +264,10 @@ if (localStorage.length > 0) {
   todoListArray.forEach((todo) => {
     createTodoList(todo);
   });
+  remainingTasks.textContent = document.querySelectorAll('.appearance').length
 }
 
-
-// localStorage.clear()
+function updateLocalStorage() {
+  localStorage.setItem('todoList', JSON.stringify(todoListArray));
+  localStorage.setItem('checked', JSON.stringify(Array.from(checkedMap.entries())))
+}
